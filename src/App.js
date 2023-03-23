dd6385a9d9bc9fa9b13b4d2eef9de46c4139210d
“Anshika <ansgupta.01@gmail.com>
2023-03-23 20:07:18 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import React, { createContext, useState } from 'react'
-import './App.css'
-import ContextApi from './ContextApi'
+import React from 'react'
+import Counter from './Counter'
 
-export const ExampleHooks = createContext();
-function App() {
-  const [color, setColor] = useState('red')
+const App = () => {
   return (
-    <ExampleHooks.Provider value={{appColor:color}}>
-    <div className='App'>
-      <h1>
-       App Component
-      </h1>
-      <ContextApi />
-      </div>
-      </ExampleHooks.Provider>
+    <div>
+      <Counter />
+      
+    </div>
   )
 }
 
