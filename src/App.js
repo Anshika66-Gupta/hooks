84b6ad9acceb82969977527f277bf7ecaa831149
“Anshika <ansgupta.01@gmail.com>
2023-03-24 19:46:27 +0530

    useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import Counter from './Counter'
+import React, { useMemo, useState } from 'react'
+
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  const [item, setItem] = useState(2);
+  
+  const multiCountMemo = useMemo(function multiCount() {
+    console.warn("multiCount")
+    return count*7
+  },[count])
   return (
     <div>
-      <Counter />
+      <h1>UseMemo Hooks</h1>
       
+      <h2>Count : {count}</h2>
+      <h2>Items : {item}</h2>
+<h2>{multiCountMemo}</h2>
+      <button onClick={() => setCount(count + 1)}>Update Count</button>
+      <button onClick={()=>setItem(item*12)}>Update Item</button>
+
     </div>
   )
 }
