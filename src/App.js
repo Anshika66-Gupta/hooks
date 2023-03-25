693fb7289f99d1f6a7dbd37feeca30731dff2f1d
“Anshika <ansgupta.01@gmail.com>
2023-03-25 19:38:33 +0530

    location pathname

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import Navbar from './Navbar'
+import About from './Pages/About'
+import Pricing from './Pages/Pricing'
 
 const App = () => {
+  let Component 
+  switch (window.location.pathname) {
+    case "/":
+      Component = <App />
+        break 
+    case "/about":
+      Component = <About />
+      break
+    case "/pricing":
+      Component = <Pricing />
+      break
+      default:
+
+}
   return (
-    <div>
+   <>
       <Navbar />
-    </div>
+      {Component} 
+    </>
   )
 }
 
